8029c9c475fde50684ef19d74c523eb5bfd52b1c
haridasnykiel <haridasnykiel@gmail.com>
2023-06-06 22:11:06 +0100

    made a start on the maze solver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,6 @@
+const directions = [{col: 1, row: 0}, {col: 0, row: 1}, {col: -1, row: 0}, {col: 0, row: -1}];
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+    const path: Point[] = [];
 
+    // go through each direction and add the result to the path, that should cover the base case. Then Add the base case. 
 }
