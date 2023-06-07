09f042f6ab6b966cff3024cd74d57cc2fd875a3b
haridasnykiel <haridasnykiel@gmail.com>
2023-06-07 17:39:42 +0100

    finish the base case

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,8 +1,19 @@
 const directions = [{col: 1, row: 0}, {col: 0, row: 1}, {col: -1, row: 0}, {col: 0, row: -1}];
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
     const path: Point[] = [];
-    // go through each direction and add the result to the path, that should cover the base case. Then Add the base case. 
+    if(start.x > maze[start.y].length || start.y > maze.length ||
+        start.x < maze[start.y].length || start.y > maze.length) 
+    {
+        return path;
+    }
+
     
+    
+    
+    
+    
+    // go through each direction and add the result to the path, that should cover the base case. Then Add the base case. 
+    // recursive case
     path.push(start);
     for (let i = 0; i < directions.length; i++) {
         const direction = directions[i];
@@ -10,5 +21,8 @@ export default function solve(maze: string[], wall: string, start: Point, end: P
         var y = start.y + direction.row;
         const paths = solve(maze, wall, {x, y}, end);
         
+        path.concat(paths);
     }
+
+    return path;
 }
