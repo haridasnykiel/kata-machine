038daf1c9ae85d966d00d14f0cc61fe0b1cf1c67
Haridas Nykiel <haridasnykiel@gmail.com>
2023-10-09 21:17:42 +0100

    implemented binary tree in order traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,15 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+export default function in_order_search(head: BinaryNode<number> | null): number[] {
+    let numbers: number[] = [];
+    
+    if(!head) {
+        return numbers;
+    }
 
+    numbers = numbers.concat(in_order_search(head.left));
+
+    numbers.push(head.value);
+
+    numbers = numbers.concat(in_order_search(head.right));
+
+    return numbers;
 }
