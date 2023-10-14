74bbdb2a420ffdc3cc669989482862e5d3a66f5c
Haridas Nykiel <haridasnykiel@gmail.com>
2023-10-14 13:32:31 +0100

    More algos

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,7 +1,20 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+export default function bfs(head: BinaryNode<number> | null, needle: number): boolean {
     const q = [head];
 
     while (q.length) {
-        
+        var item = q.shift() as BinaryNode<number> | null;
+
+        if(!item) {
+            continue;
+        }
+
+        if(item.value === needle) {
+            return true;
+        }
+
+        q.push(item.left);
+        q.push(item.right);
     }
+
+    return false;
 }
--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,36 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+// export default function compare(a: BinaryNode<number> | null | undefined, b: BinaryNode<number> | null | undefined): boolean {
+//     if(a?.value !== b?.value) {
+//         return false;
+//     }
+    
+//     let result: boolean = true;
 
+//     if(a?.left !== null) {
+//         result = compare(a?.left, b?.left);
+//     }
+
+//     if(a?.right !== null) {
+//         result = compare(a?.right, b?.right);
+//     }
+    
+//     return result;
+// }
+
+
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if(a === null && b === null) {
+        return true;
+    }
+
+    // this is a structural check
+    if(a === null || b === null) {
+        return false;
+    }
+
+    // this checks the values
+    if(a.value !== b.value) {
+        return false;
+    }
+    
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
