200dfcc1740e89f9bcdaaad7300e8a9c1e738208
Haridas.Nykiel <Haridas.Nykiel@jet2.com>
2023-10-27 14:22:39 +0100

    updated some formatting

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,18 +1,17 @@
 export default class Trie {
     
-
-    
-
     constructor() {
     }
 
     insert(item: string): void {
 
-}
+    }
+
     delete(item: string): void {
 
-}
+    }
+
     find(partial: string): string[] {
 
-}
+    }
 }
