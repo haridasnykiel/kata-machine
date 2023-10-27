9c7a142ec80e1b842ccde881b7dd3a41b555181b
Haridas Nykiel <haridasnykiel@gmail.com>
2023-10-27 18:38:09 +0100

    started to implement the Trie Tree ds

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,17 +1,54 @@
+type TrieNode = {
+    value: string | null;
+    children: TrieNode[];
+    isWord: boolean;
+}
 export default class Trie {
-    
+    private length: number;
+    private head: TrieNode;
+
     constructor() {
+        this.length = 0;
+        this.head = {
+            value: null,
+            children: [],
+            isWord: false
+        };
     }
 
     insert(item: string): void {
-
+        let currNode = this.head;
+        for (let index = 0; index < item.length; index++) {
+            const char = item[index];
+            const numVal = char.charCodeAt(0);
+            const child = currNode.children[numVal];
+            
+            if(child) {
+                currNode = child;
+            } else {
+                const newNode: TrieNode = { 
+                    value: char,
+                    children: [],
+                    isWord: false
+                };
+                currNode.children[numVal] = newNode;
+                currNode = newNode;
+            }
+        }
+        currNode.isWord = true;
     }
 
     delete(item: string): void {
 
     }
 
-    find(partial: string): string[] {
+    find(partial: string, index: number, currNode: TrieNode): string[] {
+        const results: string[] = [];
+        let charVal = partial[index].charCodeAt(0);
+        //currNode.children[part]
+        
 
+        
+        return results;
     }
 }
