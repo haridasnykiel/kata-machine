655525d873ea84fb3ec5630b45643e09a115e40c
Haridas.Nykiel <Haridas.Nykiel@jet2.com>
2023-11-06 16:43:04 +0000

    First attempt at the BFS for a Graph Matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -4,5 +4,27 @@ export default function bfs(
     needle: number): number[] | null {
     // source is the starting node, needle is what we are looking for. 
     // Return the path taken to get to the needle.
+    const queue: number[] = [source];
+    const visited: boolean[] = [true];
+    const path: number[] = [0];
+    do {
+        const node = queue.shift() as number;
+        const edges = graph[node];
+        if(node === needle) {
+            break;
+        }
+        for (let idx = 0; idx < edges.length; idx++) {
+            const value = edges[idx];
+            if(visited[idx]) {
+                continue;
+            }
+            visited[idx] = true;
+            path[idx] = value;
+            if(value > 0) {
+                queue.push(idx);
+            }
+        }
+    } while (queue.length > 0)
     
+    return path;
 }
