e588079ba0bfaf1f7797d2ae0f89e6b81f401d8a
Haridas Nykiel <haridasnykiel@gmail.com>
2023-11-06 09:53:39 +0000

    Notes added to the bfs for the graph matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,8 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
-
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number): number[] | null {
+    // source is the starting node, needle is what we are looking for. 
+    // Return the path taken to get to the needle.
+    
 }
