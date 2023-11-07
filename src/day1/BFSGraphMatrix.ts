65629ab4e366629ad0452fcbc0892db1f75a78cb
Haridas Nykiel <haridasnykiel@gmail.com>
2023-11-07 22:38:53 +0000

    completed the breadth first search algo on a adj matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -5,26 +5,45 @@ export default function bfs(
     // source is the starting node, needle is what we are looking for. 
     // Return the path taken to get to the needle.
     const queue: number[] = [source];
-    const visited: boolean[] = [true];
-    const path: number[] = [0];
+    const visited: boolean[] = new Array(graph.length).fill(false);
+    const prev: number[] = new Array(graph.length).fill(-1);
+    
+    visited[source] = true;
+    
     do {
         const node = queue.shift() as number;
         const edges = graph[node];
         if(node === needle) {
             break;
         }
+        
         for (let idx = 0; idx < edges.length; idx++) {
             const value = edges[idx];
+            if(value === 0) {
+                continue;
+            }
+            
             if(visited[idx]) {
                 continue;
             }
             visited[idx] = true;
-            path[idx] = value;
-            if(value > 0) {
-                queue.push(idx);
-            }
+            prev[idx] = node;
+            
+            queue.push(idx);
         }
     } while (queue.length > 0)
     
-    return path;
+    const result: number[] = [];
+    let curr = needle;
+    
+    while(prev[curr] !== -1) {
+        result.push(curr);
+        curr = prev[curr];
+    }
+    
+    if(result.length === 0) {
+        return null;
+    }
+    
+    return [source].concat(result.reverse());
 }
