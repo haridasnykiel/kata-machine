a1227d17ec3d4823a41ef3f840f4c68261ed68d6
Haridas Nykiel <haridasnykiel@gmail.com>
2023-11-13 17:37:56 +0000

    Made a start on Dijkstra's list algo

--- src/day2/DijkstraList.ts
+++ src/day2/DijkstraList.ts
@@ -3,5 +3,16 @@ export default function dijkstra_list(
     sink: number,
     arr: WeightedAdjacencyList): number[] {
     
+    const visited: boolean[] = new Array(arr.length).fill(false);
+    const previous: number[] = new Array(arr.length).fill(-1);
+    const distances: number[] = new Array(arr.length).fill(Infinity);
     
+    distances[source] = 0;
+    
+    while (visited.includes(false)) {
+        const lowestEdgeNotVisited = arr[source]
+    }
+}
+
+function getSmallestDistanceUnvisitedEdge(visited: number[], distances: number[]): GraphEdge {
 }
