576a797364084b9ecc04c071ddcee120df088903
Haridas.Nykiel <Haridas.Nykiel@jet2.com>
2023-11-29 17:04:15 +0000

    Made a start on a new min heap that uses a node type that will include more information that can be used by the dijkstra's algo.

--- src/day2/MinHeap.ts
+++ src/day2/MinHeap.ts
@@ -1,15 +1,50 @@
-export default class MinHeap {
-    public length: number;
+type MinHeapNode<T> = {
+    node: number,
+    weight: number,
+    value: T
+}
 
-    
+export default class MinHeap<T> {
+    public length: number;
+    private data: MinHeapNode<T>[];
+    private lastNode: MinHeapNode<T>;
+    private rootNode: MinHeapNode<T>;
 
     constructor() {
+        this.length = 0;
+        this.data = [];
     }
 
-    insert(value: number): void {
+    insert(value: T): void {
 
-}
-    delete(): number {
+    }
 
-}
+    delete(): T {
+
+    }
+
+    up(): void {
+
+        if(this.lastNode === undefined) {
+            return;
+        }
+
+        const parentIdx = this.parent(this.length - 1);
+        const parent = this.data[parentIdx];
+
+        
+
+    }
+
+    left(idx: number): number {
+        return idx * 2 + 1;
+    }
+
+    right(idx: number): number {
+        return idx * 2 + 2;
+    }
+
+    parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
 }
